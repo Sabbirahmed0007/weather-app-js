5c196a07ea1d40421302719e980331b4665da83f
Sabbirahmed0007 <>
2023-12-19 06:02:29 +0600

    Added the functionalities of the app

--- app.js
+++ app.js
@@ -1,19 +1,47 @@
 // 754f46f567ced3ef6727c2841de24257
 const apiKey = '754f46f567ced3ef6727c2841de24257';
-const apiUrl = 'https://api.openweathermap.org/data/2.5/weather?units=metric&q=Sylhet';
+const apiUrl = 'https://api.openweathermap.org/data/2.5/weather?units=metric';
 
-const searchBox = document.querySelector('.search-bar')
-const searchButton = document.querySelector('.search')
-
+const searchBox = document.querySelector('.search-bar');
+const searchButton = document.querySelector('.search');
+const weatherIcon= document.querySelector('.weather-icon');
 
 async function checkWeather(city){
     const response = await fetch(`${apiUrl}&q=${city}&appid=${apiKey}`);
-    var data = await response.json();
-    console.log(data);
-    document.querySelector('.city').innerHTML = data.name;
-    document.querySelector('.temp').innerHTML = Math.round(data.main.temp) + '°C';
-    document.querySelector('.humidity').innerHTML = data.main.humidity + '%';
-    document.querySelector('.wind').innerHTML = data.wind.speed + 'km/p';
+    if(response.status === 404){
+        document.querySelector('.error').style.display= 'block';
+        document.querySelector('.weather').style.display= 'none';
+    }
+    else{
+
+        var data = await response.json();
+        console.log(data);
+        document.querySelector('.city').innerHTML = data.name;
+        document.querySelector('.temp').innerHTML = Math.round(data.main.temp) + '°C';
+        document.querySelector('.humidity').innerHTML = data.main.humidity + '%';
+        document.querySelector('.wind').innerHTML = data.wind.speed + 'km/p';
+    
+        if(data.weather[0].main === 'Clouds'){
+            weatherIcon.src = './images/clouds.png';
+        }
+        else if(data.weather[0].main === 'Clear'){
+            weatherIcon.src= './images/clear.png';
+        }
+        else if(data.weather[0].main === 'Rain'){
+            weatherIcon.src = './images/rain.png';
+        }
+        else if(data.weather[0].main === 'Drizzle'){
+            weatherIcon.src = './images/drizzle.png';
+        }
+        else if(data.weather[0].main === 'Mist'){
+            weatherIcon.src = './images/mist.png';
+        }
+        else if(data.weather[0].main === 'Haze'){
+            weatherIcon.src = './images/snow.png';
+        }
+    }
+    
+
 };
 
 
